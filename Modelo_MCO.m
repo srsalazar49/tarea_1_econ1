142d0473e2f1b8dad8888e4cd175cab8041d9a36
Tamara Munoz Ojeda <tmunozo@fen.uchile.cl>
2024-04-21 20:30:22 -0400

    borrando funcion que no es la correcta

--- Modelo_MCO.m
+++
@@ -1,8 +0,0 @@
-% A continuacion se crea una funcion de MCO generica de manera matricial,
-% el cual permite calcular los coeficientes de interes
-function [beta_gorro] = MCO(Y,X)
-
-% Por formula, beta = (X'X)^-1 (X'Y)
-beta_gorro = (X' * X)\(X' * Y);
-
-end
